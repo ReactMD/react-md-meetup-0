5c852c12d59d986fc5fdb43711ae650f1b5bab10
Mark Geraty <mark.geraty@visionistinc.com>
2016-03-11 00:10:20 -0500

    Use left, right, up, down arrow keys to navigate.

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -6,12 +6,26 @@ class Presentation extends React.Component {
   constructor (props) {
     super (props);
 
+    this._handleKeyPress = this._handleKeyPress.bind (this);
     this._nextSlide = this._nextSlide.bind (this);
     this._previousSlide = this._previousSlide.bind (this);
 
     this.state = {
       slideIndex: 0
     };
+
+    window.addEventListener ('keydown', this._handleKeyPress);
+  }
+
+  _handleKeyPress (event) {
+    // not interested when it happens in an input
+    if (event.target.type === 'textarea') {
+      return;
+    } else if (event.keyCode === 37 || event.keyCode === 38) {
+      this._previousSlide ();
+    } else if (event.keyCode === 39 || event.keyCode === 40) {
+      this._nextSlide ();
+    }
   }
 
   _previousSlide () {
