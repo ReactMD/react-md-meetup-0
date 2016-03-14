4c5203a3a05b6d85adfc8ce30fda012404acd55e
Mark Geraty <mark.geraty@visionistinc.com>
2016-03-13 23:47:03 -0400

    Updating babel slide to use bulletedList

--- src/components/Slides/Babel.jsx
+++ src/components/Slides/Babel.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
 
-import Slide from '../Presentation/Slide';
+import { BulletedList, Slide } from '../Presentation';
+
+const bullets = [
+  "Originally \"6to5\"",
+  "Goal: \"use next generation JavaScript, today\"",
+  "Transpiles ES2015, experimental language features, JSX, and more to" +
+  "a feature set that all modern browsers can consume"
+];
 
 class Babel extends React.Component {
   render () {
     return (
       <Slide title="Babel" padded={true}>
-        <ul>
-          <li>Originally &quot;6to5&quot;</li>
-          <li>Goal: &quot;use next generation JavaScript, today&quot;</li>
-          <li>
-            Transpiles ES2015, experimental language features, JSX, and more to
-            a feature set that all modern browsers can consume.
-            <br />
-            <br />
-          </li>
-          <li>
-            React is not nearly as enjoyable without Babel's help.
-          </li>
-        </ul>
+        <BulletedList items={ bullets } />
+        <div className="footer">
+          React is not nearly as enjoyable without Babel's help.
+        </div>
       </Slide>
     );
   }
