ca83694a08e2776d278880b0847595479d9ab8b2
Mark Geraty <mark.geraty@visionistinc.com>
2016-04-27 01:50:31 -0400

    Adding a way to show why changing props is a bad idea.

--- src/components/Slides/Props/Props3.jsx
+++ src/components/Slides/Props/Props3.jsx
@@ -4,6 +4,8 @@ import { Slide } from '../../Presentation';
 
 class Props3 extends React.Component {
   render () {
+    //this.props.children = "Forget Mark's advice, I'm a rebel.";
+
     return (
       <Slide title="Props">
         <ul>
@@ -11,6 +13,7 @@ class Props3 extends React.Component {
           <li>changing them is a big no-no</li>
           <li>Zuck will get mad at you</li>
           <li><del><pre>{ "this.props.title = 'a better title';"}</pre></del></li>
+          <li>Uncomment line 7 in Props3.jsx</li>
         </ul>
       </Slide>
     )
