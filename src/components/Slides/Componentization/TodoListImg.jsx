39d90156023da4f689f314e1d8ea8b1bea78c538
Josh Gordon <github@joshgordon.net>
2016-04-26 20:40:32 -0400

    New Componentization style

--- src/components/Slides/Componentization/TodoListImg.jsx
+++ src/components/Slides/Componentization/TodoListImg.jsx
@@ -5,11 +5,24 @@ import { Slide } from '../../Presentation';
 class TodoListImg extends React.Component {
   render () {
     return (
-      <Slide>
-        <img src="images/todolist.png" />
+      <Slide title="todo list">
+        <div className="todolist-container">
+          <div className="new-todo">
+            <input placeholder="What do you have to do?" />
+            <div className="button">
+              Add
+            </div>
+          </div>
+          <div className="todolist">
+            <ul>
+              <li>learn about react</li>
+              <li>go to the grocery store</li>
+            </ul>
+          </div>
+        </div>
       </Slide>
     );
   }
-};
+}
 
-export default TodoListImg;
+export default TodoListImg;;
--- src/components/Slides/Componentization/TodoListImgComponents.jsx
+++ src/components/Slides/Componentization/TodoListImgComponents.jsx
@@ -5,11 +5,24 @@ import { Slide } from '../../Presentation';
 class TodoListImgComponents extends React.Component {
   render () {
     return (
-      <Slide>
-        <img src="images/todolist-components.png" />
+      <Slide title="todo list">
+        <div className="todolist-container">
+          <div className="new-todo">
+            <input placeholder="What do you have to do?" />
+            <div className="button">
+              Add
+            </div>
+          </div>
+          <div className="todolist">
+            <ul>
+              <li style={{border: "7px solid red"}}>learn about react</li>
+              <li style={{border: "7px solid red", marginTop: "5px"}}>go to the grocery store</li>
+            </ul>
+          </div>
+        </div>
       </Slide>
     );
   }
-};
+}
 
 export default TodoListImgComponents;
